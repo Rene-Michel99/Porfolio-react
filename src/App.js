0dea029e35a9fd0f8c0fd25e2214a09a8a84b02e
Rene-Michel99 <renemichel856@gmail.com>
2022-04-15 23:04:50 -0300

    adding components portfolio and services

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
@@ -13,6 +14,7 @@ function App() {
       <Nav />
       <About />
       <Experience />
+      <Services />
       <Portfolio />
       <Contact />
       <Footer />
--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,9 +1,78 @@
 import React from "react";
+import './portfolio.css';
+import IMG1 from '../../assets/python.png';
 
+const data = [
+    {
+        id: 1,
+        image: IMG1,
+        title: 'This is a title item',
+        github: 'https://github.com/',
+        demo: 'https://github.com/'
+    },
+    {
+        id: 2,
+        image: IMG1,
+        title: 'This is a title item',
+        github: 'https://github.com/',
+        demo: 'https://github.com/'
+    },
+    {
+        id: 3,
+        image: IMG1,
+        title: 'This is a title item',
+        github: 'https://github.com/',
+        demo: 'https://github.com/'
+    },
+    {
+        id: 4,
+        image: IMG1,
+        title: 'This is a title item',
+        github: 'https://github.com/',
+        demo: 'https://github.com/'
+    },
+    {
+        id: 5,
+        image: IMG1,
+        title: 'This is a title item',
+        github: 'https://github.com/',
+        demo: 'https://github.com/'
+    },
+    {
+        id: 6,
+        image: IMG1,
+        title: 'This is a title item',
+        github: 'https://github.com/',
+        demo: ''
+    }
+]
 
 const Portfolio = () => {
     return (
-        <section id="portfolio">Portfolio</section>
+        <section id="portfolio">
+            <h5>My Recent Work</h5>
+            <h2>Portfolio</h2>
+
+            <div className="container portfolio__container">
+                {
+                    data.map(({id, image, title, github, demo}) => {
+                        return (
+                            <article key={id} className="portfolio__item">
+                                <div className="portfolio__item-image">
+                                    <img src={image} alt={title} />
+                                </div>
+                                <h3>{title}</h3>
+                                <div className="portfolio__item-cta">
+                                    <a href={github} className="btn" target="_blank" rel="noopener noreferrer">Github</a>
+                                    { demo && <a href="http://github.com" className="btn btn-primary" target="_blank" rel="noopener noreferrer">Demo</a> }
+                                </div>
+                            </article>
+                        )
+                    })
+                }
+                
+            </div>
+        </section>
     )
 }
 
