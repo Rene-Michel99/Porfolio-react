d2faf20a7f1ab0da69afa7210b856d2e18b71014
Rene-Michel99 <renemichel856@gmail.com>
2022-04-16 18:53:09 -0300

    changing title of app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
-import About from './components/about/About'
-import Experience from './components/experience/Experience'
-import Services from './components/services/Services'
-import Portfolio from './components/portfolio/Portfolio'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Services from './components/services/Services';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import { useEffect } from 'react';
 
 function App() {
   const userLang = navigator.language || navigator.userLanguage;
+  useEffect(() => {
+    document.title = "Portfolio"
+  }, [])
   return (
     <>
       <Header lang={userLang}/>
